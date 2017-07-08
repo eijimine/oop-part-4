e6592384d5c1a3e51a34921c67fc1fbc46c412e5
eiji <eijimine@gmail.com>
2017-07-08 18:32:25 -0400

    Farm complete

--- farm.rb
+++ farm.rb
@@ -2,14 +2,7 @@ require_relative 'field.rb'
 
 class Farm
 
-  CORN = 20
-  WHEAT = 30
-
-  def initialize
-
-  end
-
-  def main_menu
+  def self.main_menu
     while true # repeat indefinitely
        print_main_menu
        user_selected = gets.to_i
@@ -17,7 +10,7 @@ class Farm
      end
   end
 
-  def print_main_menu
+  def self.print_main_menu
      puts '[1] Field'
      puts '[2] Harvest'
      puts '[3] Status'
@@ -26,7 +19,7 @@ class Farm
      puts 'Enter a number: '
   end
 
-  def call_option(user_selected)
+  def self.call_option(user_selected)
     case user_selected
     when 1 then field
     when 2 then harvest
@@ -36,77 +29,33 @@ class Farm
     end
   end
 
-  def field
+  def self.field
     print 'What kind of field is it? corn or wheat?'
     field_type = gets.chomp
 
     print 'How large is the field in Hectares?'
     field_size = gets.chomp.to_i
 
-    Field.new(field_type, field_size)
-
-      else
+    if field_type == 'corn'
+    Corn.create('corn', field_size)
+    elsif field_type == 'wheat'
+    Wheat.create('wheat', field_size)
+    else
         print 'Invalid entry'
         main_menu
     end
   end
 
-
+  def self.harvest
+    Field.harvest
   end
 
-  def modify_existing_contact
-
-    print "Who's contact would you like to modify?"
-    print "Please enter their first name: "
-    name = gets.chomp
-
-
-    print "Which attribute would you like to change?"
-    attribute = gets.chomp
-
-    print "What would you like to change it to?"
-    value = gets.chomp
-
-    contacts = Contact.find_by('first_name', name)
-    new_contact = contacts[0]
-    if new_contact == nil
-      puts "Invalid entry, please start again."
-      main_menu
-    else
-    new_contact.update(attribute, value)
-  end
+  def self.status
+    Field.status
   end
 
-  def delete_contact
-    print "Please enter the name of contact you would like to delete: "
-    name = gets.chomp
-
-
-    contacts = Contact.find_by('first_name', name)
-    new_contact = contacts[0]
-    if new_contact == nil
-      puts "Invalid entry, please start again."
-      main_menu
-    else
-    new_contact.delete
+  def self.relax
+    Field.relax
   end
-  end
-
-  def display_all_contacts
-    display = Contact.all.inspect
-    puts display
-  end
-
-  def search_by_attribute
-    print "Please enter the attribute then the value to search by: "
-    attribute = gets.chomp
-    value = gets.chomp
-
-    contacts = Contact.find_by(attribute, value)
-    contacts.each do |contact|
-      puts "Contact info: #{contact.inspect}."
-    end
-  end
-
 
 end
--- field.rb
+++ field.rb
@@ -2,9 +2,10 @@ require 'pry'
 
 class Field
 
-attr_accessor :type, :size
+attr_accessor :type, :size, :yield
 
-  @@farm = []
+  @@fields = []
+  @@total_yield = 0
 
     CORN = 20
     WHEAT = 30
@@ -15,26 +16,70 @@ attr_accessor :type, :size
 
   end
 
-    def self.create(field_type, field_size)
-      field = Field.new(field_type, field_size)
-      @@farm << field
-    end
-
-    def self.harvest
-      num1 = 0
-      num2 = 0
-      @@farm.each do |arg|
-        if arg.type == 'corn'
-          num1 += arg.size * CORN
-      elsif arg.type == 'wheat'
-          num2 += arg.size * WHEAT
-       end
-       puts num1
-       puts num2
-       total = num1 + num2
-       puts total
+  def self.relax
+    sum_corn = 0
+    sum_wheat = 0
+    @@fields.each do |field|
+      if field.type == 'corn'
+      sum_corn += field.size
+    elsif field.type == 'wheat'
+      sum_wheat += field.size
       end
     end
 
+      puts "#{sum_corn} hectares of tall green stalks rustling in the breeze fill your horizon.
+          The sun hangs low, casting an orange glow on a sea of #{sum_wheat} hectares of wheat."
+  end
+
+  def self.status
+    @@fields.each do |field|
+      puts "#{field.type} is #{field.size} hectare."
+      end
+      puts "The farm has #{@@total_yield} harvested food so far."
+  end
+
+  def self.all
+    @@fields
+  end
+
+  def self.harvest
+    sum = 0
+    @@fields.each do |field|
+    puts "Harvesting #{field.yield} food from #{field.size} hectare #{field.type} field."
+    sum += field.yield
+    end
+    @@total_yield += sum
+    puts "The farm has #{sum} harvested food so far."
+
+  end
+end
+
+class Corn < Field
+
+    def initialize(field_type, field_size)
+      super(field_type, field_size)
+      @yield = field_size * CORN
+
+    end
+
+    def self.create(field_type, field_size)
+      corn = Corn.new(field_type, field_size)
+      @@fields << corn
+    end
+
+end
+
+class Wheat < Field
+
+      def initialize(field_type, field_size)
+        super(field_type, field_size)
+        @yield = field_size * WHEAT
+
+      end
+
+    def self.create(field_type, field_size)
+      wheat = Wheat.new(field_type, field_size)
+      @@fields << wheat
+    end
 
 end
